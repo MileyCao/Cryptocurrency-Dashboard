c83f52d2c72a2b8c6e0870e096939306eb902a52
MileyCao <cao4@ualberta.ca>
2022-04-13 00:28:57 -0600

    add currency converter

--- crypto-dash/src/components/CurrencyConverter.js
+++ crypto-dash/src/components/CurrencyConverter.js
@@ -1,10 +1,37 @@
 import { useState } from 'react';
+import axios from 'axios';
 import ExchangeRate from './ExchangeRate';
 
 const CurrencyConverter = () => {
   const currencies = ['ADA', 'BTC', 'CNY', 'CAD', 'ETH', 'LTC', 'USD', 'XRP'];
   const [primaryCurrency, setPrimaryCurrency] = useState(currencies[0]);
   const [secondaryCurrency, setSecondaryCurrency] = useState(currencies[0]);
+  const [amount, setAmount] = useState(0);
+
+  const convertCurrency = () => {
+    const options = {
+      method: 'GET',
+      url: 'https://alpha-vantage.p.rapidapi.com/query',
+      params: {
+        from_currency: primaryCurrency,
+        function: 'CURRENCY_EXCHANGE_RATE',
+        to_currency: secondaryCurrency,
+      },
+      headers: {
+        'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+        'X-RapidAPI-Key': 'cc8a4ee81fmshf8e5a471f316c5dp189b02jsn84406eb4c212',
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div className="currencyConverter">
       <h2>Currency Converter</h2>
@@ -15,7 +42,12 @@ const CurrencyConverter = () => {
             <tr>
               <td>Primary Currency:</td>
               <td>
-                <input type="number" name="currency-amount-1" value={''} />
+                <input
+                  type="number"
+                  name="currency-amount-1"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
               </td>
               <td>
                 <select
@@ -50,6 +82,13 @@ const CurrencyConverter = () => {
             </tr>
           </tbody>
         </table>
+        <button
+          id="convert-button"
+          className="convertButton"
+          onClick={() => convertCurrency()}
+        >
+          Convert
+        </button>
       </div>
 
       <ExchangeRate />
